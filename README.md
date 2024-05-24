9c446a9b4f800433bc84e9a137326898e1af9bf2
HECyeah <hannahecarter@gmail.com>
2024-05-24 15:26:21 +0100

    fin touches

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Cookies
+Hello and welcome Sea Biscuits - an epic sea/biscuit based adventure!
+
+Sources and websites used in the making of this fabulous cookie game:
+-	https://blog.hubspot.com/website/center-text-in-css#:~:text=To%20center%20text%20in%20CSS%2C%20use%20the%20text%2Dalign%20property, block%20elements%2C%20such%20as%20divs.
+-	https://www.shecodes.io/athena/85664-how-to-add-transparency-to-color-background
+-	https://www.w3schools.com/howto/howto_css_image_center.asp.
+-	https://www.youtube.com/watch?v=uSJXZ3LkABE
--- app.js
+++ app.js
@@ -2,15 +2,19 @@ let cookieCount = document.getElementById("count")
 let button = document.getElementById("button")
 let bubbles = document.getElementById("bubbles")
 let autoClicker = document.getElementById("Auto-Clicker")
+let enhancedOven = document.getElementById("enhancedOven")
+let cookieFarm = document.getElementById("cookieFarm")
+let robotBaker = document.getElementById("robotBaker")
+let cookieFactory = document.getElementById("cookieFactory")
+let magicFlour = document.getElementById("magicFlour")
 let cps = 1
-const audio = new Audio("https://sound-effects.bbcrewind.co.uk/search?q=07074121");
 let cookie = 0
 
 button.onclick = function () {
     cookie = cookie + 1;
     cookieCount.textContent = cookie;
 }
-// for the section abotu I foudn this tutorial really helpful as I was having truble getting the nubmer on the page. https://www.youtube.com/watch?v=uSJXZ3LkABE
+// For the section, I used the following tutorial as I was having trouble getting the number to increase - https://www.youtube.com/watch?v=uSJXZ3LkABE
 
 setInterval(function () {
     cookie = cookie + cps;
@@ -34,12 +38,59 @@ async function fetchInfo() {
 }
 fetchInfo()
 
+//Array is now in my console log. I have read the information and chosen to just use 6 of the upgrades so everything fits on the one page. I could probably grid the page and have the cook and counter on the left and the upgrades neatly stacked and slimer on the right so everything is still on the page, but I just think it looks really cool like this. I hope that’s okay.   
+
 autoClicker.onclick = function upgrade1() {
     if (cookie < 100) {
         alert("Not Enough Cookies")
         return;
     };
     cookie -= 100;
-    cps += 1
+    cps += 2
+}
+
+enhancedOven.onclick = function upgrade2() {
+    if (cookie < 500) {
+        alert("Not Enough Cookies")
+        return;
+    };
+    cookie -= 500;
+    cps += 5
+}
+
+cookieFarm.onclick = function upgrade3() {
+    if (cookie < 1000) {
+        alert("Not Enough Cookies")
+        return;
+    };
+    cookie -= 1000;
+    cps += 10
+}
+
+robotBaker.onclick = function upgrade3() {
+    if (cookie < 2000) {
+        alert("Not Enough Cookies")
+        return;
+    };
+    cookie -= 2000;
+    cps += 20
+}
+
+cookieFactory.onclick = function upgrade3() {
+    if (cookie < 5000) {
+        alert("Not Enough Cookies")
+        return;
+    };
+    cookie -= 5000;
+    cps += 50
+}
+
+magicFlour.onclick = function upgrade3() {
+    if (cookie < 10000) {
+        alert("Not Enough Cookies")
+        return;
+    };
+    cookie -= 10000;
+    cps += 100
 }
 
