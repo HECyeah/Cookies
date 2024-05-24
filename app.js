de915c3b1201cd007e0be30c6e5e3ed34d5cbbcd
HECyeah <hannahecarter@gmail.com>
2024-05-24 09:15:32 +0100

    add

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ button.onclick = function () {
     cookieCount.textContent = cookie;
 }
 // for the section abotu I foudn this tutorial really helpful as I was having truble getting the nubmer on the page. https://www.youtube.com/watch?v=uSJXZ3LkABE
+
+setInterval(function () {
+    cookie = cookie + 1;
+    cookieCount.textContent = cookie;
+}, 1000)
+
+const makeString = JSON.stringify(textContent);
+localStorage.setItem("myCoolInfo");
+const myRetriveInfo = localStorage.getItem("myCoolInfo");
+const info = JSON.parse(myRetriveInfo)
+
+async function fetchInfo() {
+    let response = await fetch("https://cookie-upgrade-api.vercel.app/api/upgrades");
+    console.log("HTTP Response:", response);
+    const json = await response.json();
+    console.log("JSON data:", json)
+}
+
+fetchInfo()
