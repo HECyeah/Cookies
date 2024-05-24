d79457a8fd100109ecb250cbf7ff28334a68c33d
HECyeah <hannahecarter@gmail.com>
2024-05-24 11:52:09 +0100

    start of upgrades

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let cookieCount = document.getElementById("count")
 let button = document.getElementById("button")
 let bubbles = document.getElementById("bubbles")
+let autoClicker = document.getElementById("Auto-Clicker")
+let cps = 1
 const audio = new Audio("https://sound-effects.bbcrewind.co.uk/search?q=07074121");
 let cookie = 0
 
@@ -11,14 +13,18 @@ button.onclick = function () {
 // for the section abotu I foudn this tutorial really helpful as I was having truble getting the nubmer on the page. https://www.youtube.com/watch?v=uSJXZ3LkABE
 
 setInterval(function () {
-    cookie = cookie + 1;
+    cookie = cookie + cps;
+    save()
     cookieCount.textContent = cookie;
 }, 1000)
 
-const makeString = JSON.stringify(textContent);
-localStorage.setItem("myCoolInfo");
-const myRetriveInfo = localStorage.getItem("myCoolInfo");
+function save() {
+    let turnNumberToString = JSON.stringify(cookie);
+    localStorage.setItem("stringCookies", turnNumberToString);
+}
+const myRetriveInfo = localStorage.getItem("stringCookies");
 const info = JSON.parse(myRetriveInfo)
+console.log(info)
 
 async function fetchInfo() {
     let response = await fetch("https://cookie-upgrade-api.vercel.app/api/upgrades");
@@ -26,5 +32,14 @@ async function fetchInfo() {
     const json = await response.json();
     console.log("JSON data:", json)
 }
-
 fetchInfo()
+
+autoClicker.onclick = function upgrade1() {
+    if (cookie < 100) {
+        alert("Not Enough Cookies")
+        return;
+    };
+    cookie -= 100;
+    cps += 1
+}
+
